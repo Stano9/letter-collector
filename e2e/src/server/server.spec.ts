1f21a7866c949cfc70f84e646b69296e1b9093c7
Stan <stano.naumoski@hotmail.com>
2024-02-23 16:23:32 +0100

    Goonies test

--- e2e/src/server/server.spec.ts
+++ e2e/src/server/server.spec.ts
@@ -1,10 +1,9 @@
-import axios from 'axios';
-
-describe('GET /', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/`);
-
-    expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
-  });
-});
+import { collectLetters } from 'src/services/collectLetters.service'
+import { GOONIES } from 'src/helpers/maps'
+describe('Goonies Map', () => {
+  it('It should return GOONIES', async () => {
+    const result = collectLetters(GOONIES)
+    console.log(result)
+    expect(result).toEqual({ letters: 'GOONIES', path: '@GO||+-O-N+|I|+-+|+-i+|ES|x' })
+  })
+})
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { collectLetters } from './services/collectLetters.service'
+import { GOONIES } from './helpers/maps'
 
 const host = process.env.HOST ?? 'localhost'
 const port = process.env.PORT ? Number(process.env.PORT) : 3000
@@ -7,25 +8,8 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000
 const app = express()
 
 app.get('/', (req, res) => {
-  const map = [
-    ['@', '-', '-', '-', 'A', '-', '-', '-', '+'],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'],
-    ['x', '-', 'B', '-', '+', ' ', ' ', ' ', 'C'],
-    [' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|'],
-    [' ', ' ', ' ', ' ', '+', '-', '-', '-', '+'],
-  ]
-  const map2 = [
-    [' ', ' ', '+', '-', 'O', '-', 'N', '+', ' ', ' ', ' '],
-    [' ', ' ', '|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' '],
-    [' ', ' ', '|', ' ', ' ', '+', '-', 'I', '+', ' ', ' '],
-    ['@', 'G', 'O', '-', '+', '|', ' ', '|', '|', ' ', ' '],
-    [' ', ' ', '|', ' ', '|', '+', '-', '+', 'E', ' ', ' '],
-    [' ', ' ', '+', '-', '+', ' ', ' ', ' ', 'S', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x', ' ', ' '],
-  ]
   try {
-    const result = collectLetters(map2)
+    const result = collectLetters(GOONIES)
     res.send(result)
   } catch (error) {
     console.log(error)
